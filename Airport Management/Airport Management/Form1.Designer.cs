37403c3cc2262263d537704af3f8971e5062a107
mateopag <mateopag98@gmail.com>
2017-06-11 15:45:36 -0300

    Algunas interfaces del programa

---
+++ Airport Management/Airport Management/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace Airport_Management
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.aVIONESToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.vUELOSToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pLANESToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rEPORTESToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cONFIGURACIONToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.agregarAviónToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.modificarAviónToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eliminarAviónToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.agregarVueloToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.modificarVueloToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eliminarVueloToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.generarPlanToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aVIONESToolStripMenuItem,
+            this.vUELOSToolStripMenuItem,
+            this.pLANESToolStripMenuItem,
+            this.rEPORTESToolStripMenuItem,
+            this.cONFIGURACIONToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(454, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // aVIONESToolStripMenuItem
+            // 
+            this.aVIONESToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.agregarAviónToolStripMenuItem,
+            this.modificarAviónToolStripMenuItem,
+            this.eliminarAviónToolStripMenuItem});
+            this.aVIONESToolStripMenuItem.Name = "aVIONESToolStripMenuItem";
+            this.aVIONESToolStripMenuItem.Size = new System.Drawing.Size(66, 20);
+            this.aVIONESToolStripMenuItem.Text = "AVIONES";
+            // 
+            // vUELOSToolStripMenuItem
+            // 
+            this.vUELOSToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.agregarVueloToolStripMenuItem,
+            this.modificarVueloToolStripMenuItem,
+            this.eliminarVueloToolStripMenuItem});
+            this.vUELOSToolStripMenuItem.Name = "vUELOSToolStripMenuItem";
+            this.vUELOSToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.vUELOSToolStripMenuItem.Text = "VUELOS";
+            // 
+            // pLANESToolStripMenuItem
+            // 
+            this.pLANESToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.generarPlanToolStripMenuItem});
+            this.pLANESToolStripMenuItem.Name = "pLANESToolStripMenuItem";
+            this.pLANESToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.pLANESToolStripMenuItem.Text = "PLANES";
+            // 
+            // rEPORTESToolStripMenuItem
+            // 
+            this.rEPORTESToolStripMenuItem.Name = "rEPORTESToolStripMenuItem";
+            this.rEPORTESToolStripMenuItem.Size = new System.Drawing.Size(73, 20);
+            this.rEPORTESToolStripMenuItem.Text = "REPORTES";
+            // 
+            // cONFIGURACIONToolStripMenuItem
+            // 
+            this.cONFIGURACIONToolStripMenuItem.Name = "cONFIGURACIONToolStripMenuItem";
+            this.cONFIGURACIONToolStripMenuItem.Size = new System.Drawing.Size(114, 20);
+            this.cONFIGURACIONToolStripMenuItem.Text = "CONFIGURACIÓN";
+            // 
+            // agregarAviónToolStripMenuItem
+            // 
+            this.agregarAviónToolStripMenuItem.Name = "agregarAviónToolStripMenuItem";
+            this.agregarAviónToolStripMenuItem.Size = new System.Drawing.Size(157, 22);
+            this.agregarAviónToolStripMenuItem.Text = "Agregar avión";
+            // 
+            // modificarAviónToolStripMenuItem
+            // 
+            this.modificarAviónToolStripMenuItem.Name = "modificarAviónToolStripMenuItem";
+            this.modificarAviónToolStripMenuItem.Size = new System.Drawing.Size(157, 22);
+            this.modificarAviónToolStripMenuItem.Text = "Modificar avión";
+            // 
+            // eliminarAviónToolStripMenuItem
+            // 
+            this.eliminarAviónToolStripMenuItem.Name = "eliminarAviónToolStripMenuItem";
+            this.eliminarAviónToolStripMenuItem.Size = new System.Drawing.Size(157, 22);
+            this.eliminarAviónToolStripMenuItem.Text = "Eliminar avión";
+            // 
+            // agregarVueloToolStripMenuItem
+            // 
+            this.agregarVueloToolStripMenuItem.Name = "agregarVueloToolStripMenuItem";
+            this.agregarVueloToolStripMenuItem.Size = new System.Drawing.Size(157, 22);
+            this.agregarVueloToolStripMenuItem.Text = "Agregar vuelo";
+            // 
+            // modificarVueloToolStripMenuItem
+            // 
+            this.modificarVueloToolStripMenuItem.Name = "modificarVueloToolStripMenuItem";
+            this.modificarVueloToolStripMenuItem.Size = new System.Drawing.Size(157, 22);
+            this.modificarVueloToolStripMenuItem.Text = "Modificar vuelo";
+            // 
+            // eliminarVueloToolStripMenuItem
+            // 
+            this.eliminarVueloToolStripMenuItem.Name = "eliminarVueloToolStripMenuItem";
+            this.eliminarVueloToolStripMenuItem.Size = new System.Drawing.Size(157, 22);
+            this.eliminarVueloToolStripMenuItem.Text = "Eliminar vuelo";
+            // 
+            // generarPlanToolStripMenuItem
+            // 
+            this.generarPlanToolStripMenuItem.Name = "generarPlanToolStripMenuItem";
+            this.generarPlanToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.generarPlanToolStripMenuItem.Text = "Generar plan";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(454, 421);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem aVIONESToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem agregarAviónToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem modificarAviónToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eliminarAviónToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem vUELOSToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem agregarVueloToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem modificarVueloToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eliminarVueloToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pLANESToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem generarPlanToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem rEPORTESToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cONFIGURACIONToolStripMenuItem;
+
+    }
+}
+
---
+++ Airport Management/Airport Management/agregarAvion.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace Airport_Management
+{
+    partial class agregarAvion
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Código:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tipo:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(82, 33);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(152, 26);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(82, 97);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(152, 26);
+            this.textBox2.TabIndex = 3;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(17, 162);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(217, 35);
+            this.btnAgregar.TabIndex = 4;
+            this.btnAgregar.Text = "Agregar avión";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // agregarAvion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(260, 229);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "agregarAvion";
+            this.Text = "agregarAvion";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnAgregar;
+    }
+}
