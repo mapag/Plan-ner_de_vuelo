1af9c91286eab92ba84d11c4320d7f8fbf381711
N41MaD <damian.valles@protonmail.com>
2017-06-12 09:10:05 -0300

    Se enlazan las diferentes ventanas

--- Airport Management/Airport Management/Form1.cs
+++ Airport Management/Airport Management/Form1.cs
@@ -16,5 +16,77 @@ namespace Airport_Management
         {
             InitializeComponent();
         }
+
+        private void agregarAviónToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < this.MdiChildren.Length; i++)
+            {
+                this.MdiChildren[i].Close();
+            }
+            
+            agregarAvion agreAvion = new agregarAvion();
+            agreAvion.MdiParent = this;
+            agreAvion.Show();
+        }
+
+        private void modificarAviónToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < this.MdiChildren.Length; i++)
+            {
+                this.MdiChildren[i].Close();
+            }
+
+            modificarAvion2 modAvion = new modificarAvion2();
+            modAvion.MdiParent = this;
+            modAvion.Show();
+        }
+
+        private void eliminarAviónToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < this.MdiChildren.Length; i++)
+            {
+                this.MdiChildren[i].Close();
+            }
+
+            eliminarAvion elimAvion = new eliminarAvion();
+            elimAvion.MdiParent = this;
+            elimAvion.Show();
+        }
+
+        private void agregarVueloToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < this.MdiChildren.Length; i++)
+            {
+                this.MdiChildren[i].Close();
+            }
+
+            agregarVuelo agregVuelo = new agregarVuelo();
+            agregVuelo.MdiParent = this;
+            agregVuelo.Show();
+        }
+
+        private void modificarVueloToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < this.MdiChildren.Length; i++)
+            {
+                this.MdiChildren[i].Close();
+            }
+
+            modificarVuelo modVuelo = new modificarVuelo();
+            modVuelo.MdiParent = this;
+            modVuelo.Show();
+        }
+
+        private void eliminarVueloToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < this.MdiChildren.Length; i++)
+            {
+                this.MdiChildren[i].Close();
+            }
+
+            eliminarVuelo elimVuelo = new eliminarVuelo();
+            elimVuelo.MdiParent = this;
+            elimVuelo.Show();
+        }
     }
 }
---
+++ Airport Management/Airport Management/modificarAvion2.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Airport_Management
+{
+    partial class modificarAvion2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Location = new System.Drawing.Point(14, 105);
+            this.btnModificar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(170, 24);
+            this.btnModificar.TabIndex = 5;
+            this.btnModificar.Text = "Modificar avion";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 33);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Código:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 70);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Tipo:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(81, 30);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(103, 20);
+            this.textBox1.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(81, 67);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(103, 20);
+            this.textBox2.TabIndex = 9;
+            // 
+            // modificarAvion2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(199, 160);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnModificar);
+            this.Name = "modificarAvion2";
+            this.Text = "modificarAvion2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
