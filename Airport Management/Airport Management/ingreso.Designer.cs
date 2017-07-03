c0ee4e1d2b8f898ff84c7b493d375a729b328006
mateopag <mateopag98@gmail.com>
2017-07-03 11:08:06 -0300

    Ingreso funciona

--- Airport Management/Airport Management/ingreso.Designer.cs
+++ Airport Management/Airport Management/ingreso.Designer.cs
@@ -38,6 +38,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(12, 80);
             this.label1.Name = "label1";
@@ -48,6 +49,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(12, 138);
             this.label2.Name = "label2";
@@ -88,6 +90,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightCyan;
+            this.BackgroundImage = global::Airport_Management.Properties.Resources.fondo;
             this.ClientSize = new System.Drawing.Size(430, 262);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.txtContra);
