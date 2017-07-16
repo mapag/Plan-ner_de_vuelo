a0412d004ad2cf56aba5afb93c89dd249e34e98a
mateopag <mateopag98@gmail.com>
2017-07-16 15:17:54 -0300

    + en usuarios

--- Airport Management/Airport Management/ingreso.cs
+++ Airport Management/Airport Management/ingreso.cs
@@ -15,28 +15,25 @@ namespace Airport_Management
     public partial class ingreso : Form
     {
         AccesoDatos ad = new AccesoDatos();
-        public bool validado = false;
+        public int validado = 0;
         public ingreso()
         {
             InitializeComponent();
         }
-        private void sistema()
-        {
-            validado = true;
-            this.Close();
-        }
+
         private void button1_Click(object sender, EventArgs e)
         {
-            int cant = ad.ContarRegistros("select usuario from usuarios where usuario = '" + txtUsuario.Text + "' and contrasena = '" + txtContra.Text + "'");
-            
-            //if (cant == 1)
-            //{
-                sistema();
-           // }
-            //else
-            //{
-            //    MessageBox.Show("Usuario o contraseña inexistente");
-            //}
+            int cant = ad.ContarRegistros("select id from usuarios where usuario = '" + txtUsuario.Text + "' and contrasena = '" + txtContra.Text + "'");
+
+            if (cant == 1)
+            {
+                validado = 123;
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Usuario o contraseña inexistente");
+            }
         }
     }
 }
--- Airport Management/Airport Management/verPerfil.Designer.cs
+++ Airport Management/Airport Management/verPerfil.Designer.cs
@@ -28,89 +28,98 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.imgFoto = new System.Windows.Forms.PictureBox();
+            this.txtNombre = new System.Windows.Forms.Label();
+            this.txtApellido = new System.Windows.Forms.Label();
+            this.txtCorreo = new System.Windows.Forms.Label();
+            this.txtTelefono = new System.Windows.Forms.Label();
+            this.txtUsuario = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.imgFoto)).BeginInit();
             this.SuspendLayout();
             // 
-            // pictureBox1
+            // imgFoto
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(333, 50);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(176, 165);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.imgFoto.Location = new System.Drawing.Point(298, 47);
+            this.imgFoto.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.imgFoto.Name = "imgFoto";
+            this.imgFoto.Size = new System.Drawing.Size(117, 107);
+            this.imgFoto.TabIndex = 0;
+            this.imgFoto.TabStop = false;
             // 
-            // label1
+            // txtNombre
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(27, 72);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(79, 24);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Nombre";
+            this.txtNombre.AutoSize = true;
+            this.txtNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNombre.Location = new System.Drawing.Point(18, 47);
+            this.txtNombre.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(79, 24);
+            this.txtNombre.TabIndex = 1;
+            this.txtNombre.Text = "Nombre";
             // 
-            // label2
+            // txtApellido
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(27, 134);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(79, 24);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Apellido";
+            this.txtApellido.AutoSize = true;
+            this.txtApellido.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtApellido.Location = new System.Drawing.Point(18, 111);
+            this.txtApellido.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(79, 24);
+            this.txtApellido.TabIndex = 2;
+            this.txtApellido.Text = "Apellido";
             // 
-            // label3
+            // txtCorreo
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(27, 290);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(68, 24);
-            this.label3.TabIndex = 3;
-            this.label3.Text = "Correo";
+            this.txtCorreo.AutoSize = true;
+            this.txtCorreo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCorreo.Location = new System.Drawing.Point(18, 261);
+            this.txtCorreo.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.txtCorreo.Name = "txtCorreo";
+            this.txtCorreo.Size = new System.Drawing.Size(78, 24);
+            this.txtCorreo.TabIndex = 3;
+            this.txtCorreo.Text = "Correo: ";
             // 
-            // label4
+            // txtTelefono
             // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(27, 353);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(85, 24);
-            this.label4.TabIndex = 4;
-            this.label4.Text = "Telefono";
+            this.txtTelefono.AutoSize = true;
+            this.txtTelefono.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTelefono.Location = new System.Drawing.Point(18, 314);
+            this.txtTelefono.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.txtTelefono.Name = "txtTelefono";
+            this.txtTelefono.Size = new System.Drawing.Size(95, 24);
+            this.txtTelefono.TabIndex = 4;
+            this.txtTelefono.Text = "Telefono: ";
             // 
-            // label5
+            // txtUsuario
             // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(390, 233);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(72, 24);
-            this.label5.TabIndex = 5;
-            this.label5.Text = "usuario";
+            this.txtUsuario.AutoSize = true;
+            this.txtUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsuario.Location = new System.Drawing.Point(320, 170);
+            this.txtUsuario.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(72, 24);
+            this.txtUsuario.TabIndex = 5;
+            this.txtUsuario.Text = "usuario";
             // 
             // verPerfil
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(541, 453);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.pictureBox1);
+            this.BackColor = System.Drawing.Color.LightCyan;
+            this.ClientSize = new System.Drawing.Size(479, 391);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.txtTelefono);
+            this.Controls.Add(this.txtCorreo);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.imgFoto);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "verPerfil";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "verPerfil";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.Load += new System.EventHandler(this.verPerfil_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imgFoto)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -118,11 +127,11 @@
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox imgFoto;
+        private System.Windows.Forms.Label txtNombre;
+        private System.Windows.Forms.Label txtApellido;
+        private System.Windows.Forms.Label txtCorreo;
+        private System.Windows.Forms.Label txtTelefono;
+        private System.Windows.Forms.Label txtUsuario;
     }
 }
